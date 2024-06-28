9e8cde3badefd1791a4f6e7ec6a36bc12bf6780a
Jalalgorithm <temitomzi@gmail.com>
2024-06-28 01:04:04 +0100

    Boilerplate

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentHome.Shared.Models;
+
+namespace RentHome.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<House>  Houses { get; set; }
+    }
+}
---
+++ Shared/Models/House.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentHome.Shared.Models
+{
+    public class House
+    {
+        public int Id { get; set; }
+        public string? Mode { get; set; }
+        public string? Type { get; set; }
+        public string? Location { get; set; }
+        public decimal Price { get; set; }
+        public int Size { get; set; }
+        public int NumberOfBedroom { get; set; }
+        public int NumberOfBathroom { get; set; }
+        public string? Image { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
