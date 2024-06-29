6ab02d03cb31666da99427d20cd514736f17f3be
Jalalgorithm <temitomzi@gmail.com>
2024-06-29 10:52:52 +0100

    Linking the Server with Client

---
+++ Client/Services/HouseServices/HouseService.cs
@@ -0,0 +1,55 @@
+﻿using RentHome.Shared.CustomResponse;
+using RentHome.Shared.DTOs;
+using System.Net.Http.Json;
+
+namespace RentHome.Client.Services.HouseServices
+{
+    public class HouseService : IHouseService
+    {
+        private readonly HttpClient httpClient;
+
+        public HouseService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<Response> AddHouseData(HouseRequestDTO houseRequest)
+        {
+            var result = await httpClient.PostAsJsonAsync("api/house", houseRequest);
+            var response = await result.Content.ReadFromJsonAsync<Response>();
+
+            return response!;
+        }
+
+        public async Task<Response> DeleteHouseData(int id)
+        {
+            var result = await httpClient.DeleteAsync($"api/house/{id}");
+            var response = await result.Content.ReadFromJsonAsync<Response>();
+
+            return response!;
+        }
+
+        public async Task<List<HouseResponseDTO>> GetAllHouseData()
+        {
+            var result = await httpClient.GetAsync("api/house");
+            var response = await result.Content.ReadFromJsonAsync<List<HouseResponseDTO>>();
+
+            return response!;
+        }
+
+        public async Task<HouseResponseDTO> GetHouseData(int id)
+        {
+            var result = await httpClient.GetAsync($"api/house/{id}");
+            var response = await result.Content.ReadFromJsonAsync<HouseResponseDTO>();
+
+            return response!;
+        }
+
+        public async Task<Response> UpdateHouseData(HouseRequestDTO houseRequest, int id)
+        {
+            var result = await httpClient.PutAsJsonAsync($"api/house/{id}", houseRequest);
+            var response = await result.Content.ReadFromJsonAsync<Response>();  
+
+            return response!;
+        }
+    }
+}
---
+++ Client/Services/HouseServices/IHouseService.cs
@@ -0,0 +1,14 @@
+﻿using RentHome.Shared.CustomResponse;
+using RentHome.Shared.DTOs;
+
+namespace RentHome.Client.Services.HouseServices
+{
+    public interface IHouseService
+    {
+        Task<Response> AddHouseData(HouseRequestDTO houseRequest);
+        Task<Response> UpdateHouseData(HouseRequestDTO houseRequest , int id);
+        Task<Response> DeleteHouseData(int id);
+        Task<HouseResponseDTO> GetHouseData(int id);
+        Task<List<HouseResponseDTO>> GetAllHouseData();
+    }
+}
